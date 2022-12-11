4a041f9e929e3dfc7b5f18bb71825ff6e7345074
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-11 10:48:43 +0500

    context

---
+++ src/context/context.js
@@ -0,0 +1,17 @@
+import { createContext, useReducer } from "react";
+import { reducer } from "./reducer";
+
+export const GlobalContext = createContext("Initial Value");
+
+let data = {
+  num: 0,
+};
+
+export default function ContextProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, data);
+  return (
+    <GlobalContext.Provider value={{ state, dispatch }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}

0048eaf8debd2874d855b29cfe70195d6fecf815
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-11 12:08:23 +0500

    added condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { GlobalContext } from "./context/context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Counter from "./components/counter";
 import User from "./components/user";
 import "./App.css";
 
 function App() {
   const { state, dispatch } = useContext(GlobalContext);
+  const [isCounter, setIsCounter] = useState(false);
   const darkMode = () => {
     dispatch({
       type: "DARK_MODE",
     });
     console.log(state.darkMode);
   };
-  return (  
+  return (
     <div className={`App ${state.darkMode ? "App-bg" : ""}`}>
-        <button onClick={darkMode}>Turn {state.darkMode  ? "Off" : "On"} Dark Mode</button>
-        {/* <Counter /> */}
-        <User />
+      <button onClick={darkMode}>
+        Turn {state.darkMode ? "Off" : "On"} Dark Mode
+      </button>
+      {isCounter ? <Counter /> : <User />}
+      <br />
+
+      <button onClick={() => setIsCounter(!isCounter)}>Show {isCounter ? "User" : "Counter"}</button>
     </div>
   );
 }
